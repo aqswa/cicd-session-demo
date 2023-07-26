e1b3554bcde430ed9483cc48495f9e2f67f3d6b5
aqswa <joayoung3@gmail.com>
2023-07-26 18:24:08 +0900

    chore: test commit

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -12,6 +12,6 @@ public class SampleController {
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
     public String create() {
-        return "sample";
+        return "samples";
     }
 }
