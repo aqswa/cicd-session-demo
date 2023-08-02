04aa58727776ade0603b681980e294bc7aa7ddab
aqswa <joayoung3@gmail.com>
2023-08-02 10:14:35 +0900

    chore: deploy.yml 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd /home/ubuntu/app
 DOCKER_APP_NAME=spring
 
 # 실행중인 blue가 있는지
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 echo "$EXIST_BLUE" >> debug.log
 
 # green이 실행중이면 blue up
